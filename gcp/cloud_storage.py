258fb4046be7d066e18a84783ee2e1e8b07e7a5d
Alejandra Uniandes <ma.ninog12@uniandes.edu.co>
2023-11-08 21:41:07 -0500

    Cloud Storage: Covertir archivo y obtener archivo

---
+++ gcp/cloud_storage.py
@@ -0,0 +1,35 @@
+
+import io
+import os
+from google.cloud import storage
+
+
+bucket_name = os.environ.get("BUCKET_NAME", "proyecto-conversion")
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] ='credentials.json'
+storage_client =storage.Client()
+
+BLOB_FORMAT = "{}/{}/{}/{}.{}"
+
+
+def upload_to_bucket(blob_name,file_name):
+    try:
+        bucket = storage_client.get_bucket(bucket_name)
+        blob = bucket.blob(blob_name)
+        blob.upload_from_filename(file_name)
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+
+def download_file_from_bucket(blob_name,file_path):
+    try:
+        bucket = storage_client.get_bucket(bucket_name)
+        blob = bucket.blob(blob_name)
+        with open(file_path, 'wb') as archivo:
+            storage_client.download_blob_to_file(blob,archivo)
+        
+        return True
+    except Exception as e:
+        print(e)
+        return False
--- worker.py
+++ worker.py
@@ -2,10 +2,12 @@ from datetime import datetime
 import os
 from celery import Celery
 from convert_video import convert_video
+from gcp.cloud_storage import BLOB_FORMAT, download_file_from_bucket, upload_to_bucket
 from models import Status, Task, session
 
 broker = os.environ.get("REDIS_CONN", "redis://localhost:6379/0")
 celery = Celery("tasks", broker=broker)
+upload_folder = os.environ.get("UPLOAD_FOLDER", "videos")
 
 
 @celery.task(name="process_video")
@@ -15,11 +17,37 @@ def process_video(task_id):
         task.inprocess = datetime.utcnow()
         task.status = Status.INPROCESS
         session.commit()
-        os.makedirs(os.path.join("videos", str(task.user_id), "output"), exist_ok=True)
-        convert_video(
-            "videos/{}/input/{}.{}".format(task.user_id, task.id, task.input_format.value),
-            "videos/{}/output/{}.{}".format(task.user_id, task.id, task.output_format.value),
+
+        file_name_input = "{}.{}".format(task.id, task.input_format.value)
+        file_name_output = "{}.{}".format(task.id, task.output_format.value)
+        input_file_name = os.path.join(
+            "videos", str(task.user_id), "input", file_name_input
         )
+        output_file_name = os.path.join(
+            "videos", str(task.user_id), "output", file_name_output
+        )
+
+        input_blob_name = BLOB_FORMAT.format(
+            upload_folder, str(task.user_id), "input", task.id, task.input_format.value
+        )
+        output_blob_name = BLOB_FORMAT.format(
+            upload_folder,
+            str(task.user_id),
+            "output",
+            task.id,
+            task.output_format.value,
+        )
+
+        os.makedirs(os.path.join("videos", str(task.user_id), "input"), exist_ok=True)
+        download_file_from_bucket(input_blob_name, input_file_name)
+
+        convert_video(input_file_name, output_file_name)
+
+        os.makedirs(os.path.join("videos", str(task.user_id), "output"), exist_ok=True)
+        upload_to_bucket(output_blob_name, output_file_name)
+
+        os.remove(input_file_name)
+        os.remove(output_file_name)
         task.processed = datetime.utcnow()
         task.status = Status.PROCESSED
         session.commit()
